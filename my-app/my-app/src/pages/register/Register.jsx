415f1b641edd0aad2a35ceb81ca7dc0357734e72
Sofi <sofi.harutyunyan23@gmail.com>
2023-06-19 11:35:22 +0400

    added login & register page

---
+++ my-app/my-app/src/pages/register/Register.jsx
@@ -0,0 +1,53 @@
+import react from "react";
+import "./Register.css"
+
+
+export default function Register(props) {
+    return (
+        <div className="Auth-form-container">
+            <form className="Auth-form">
+                <div className="Auth-form-content">
+                    <h3 className="Auth-form-title">Sign Up</h3>
+                    <div className="form-group mt-3">
+                        <label>Name</label>
+                        <input
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="Name"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Phone Number</label>
+                        <input
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder="Phone Number"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Email address</label>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder="Enter email"
+                        />
+                    </div>
+                    <div className="form-group mt-3">
+                        <label>Password</label>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder="Enter password"
+                        />
+                    </div>
+                    <div className="d-grid gap-2 mt-3">
+                        <button type="submit" className="btn btn-primary">
+                            Submit
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    )
+}
+
--- my-app/my-app/src/webFront/components/relatedProducts/RelatedProducts.js
+++ my-app/my-app/src/webFront/components/relatedProducts/RelatedProducts.js
@@ -2,49 +2,29 @@ import React from "react";
 import "react-multi-carousel/lib/styles.css";
 import './RelatedProducts.css';
 import Slide from "../slide/Slide";
+import { useState, useEffect } from "react";
+import axios from "axios"
 
 
 
-function RelatedProducts() {
-    let arr = [
-        {
-            id: 1,
-            title: "FLORAL PRINT BLUE DRESS ",
-            img: "https://shop.beyondretro.com/cdn/shop/products/beyond-retro-label-womens-floral-print-blue-dress-1-E00897474_1512x.jpg?v=1685653545",
-            price: "45",
-            sale: "37",
-        },
-        {
-            id: 2,
-            title: "LIGHT BLUE DRESS",
-            img: "https://shop.beyondretro.com/cdn/shop/products/beyond-retro-label-womens-asymmetric-light-blue-dress-1-E00897737_d7b749c4-9cc7-4bce-8b36-9f38a55ec105_720x.jpg?v=1685604669",
-            price: "45",
-            sale: "40",
-        },
-        {
-            id: 3,
-            title: "HARLEY DAVIDSON  Y2K VEST",
-            img: "https://shop.beyondretro.com/cdn/shop/products/beyond-retro-label-womens-harley-davidson-embroidered-vest-1-E00825495_d2144ef6-837b-48a0-81ba-9695b43e5917_720x.jpg?v=1685511633",
-            price: "45",
-            sale: "38",
-        },
-        {
-            id: 4,
-            title: "Y2K BAGUETTE BAG ",
-            img: "https://shop.beyondretro.com/cdn/shop/products/beyond-retro-label-womens-diamonte-detail-y2k-leather-baguette-bag-1-E00916387_1512x.jpg?v=1682456919",
-            price: "47",
-            sale: "30",
-        }
+function RelatedProducts({ category }) {
 
-        
-    ];
+
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:4500/api/item?limit=4&category=" + category)
+            .then(response => setData(response.data))
+    }, [category])
+
     return (
         <div>
-        <h1 className="special-offers">Related  Products</h1>
-        <div className="offers ">
-            <Slide arr={arr} />
+            <h1 className="special-offers">Related  Products</h1>
+            <div className="offers ">
+                <Slide arr={data} />
+            </div>
         </div>
-    </div>
     )
 }
 
