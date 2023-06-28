f5df96a01c413cf5b273c9145e1ad869c4033954
Sofi <sofi.harutyunyan23@gmail.com>
2023-06-28 07:57:24 +0400

    changed showmore-category,menu,error

--- my-app/my-app/src/App.js
+++ my-app/my-app/src/App.js
@@ -1,53 +1,49 @@
-// import './App.css';
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Web from './pages/Web.js';
-import Details from './webFront/components/details/Details';
+import Web from "./pages/Web.js";
+import Details from "./webFront/components/details/Details";
 import Footer from "./webFront/components/footer/Footer.js";
 import Header from "./webFront/components/header/Header.js";
 import Login from "./pages/login/Login.jsx";
 import Register from "./pages/register/Register.jsx";
 import Basket from "./pages/basket/Basket.js";
-import Cosmetics from "./webFront/components/cosmetics/Cosmetics.js";
 import ShowMore from "./webFront/components/showmore/Showmore.js";
-import SkinCare from "./webFront/components/scincare/SkinCare.js";
-import Clothes from "./webFront/components/clothes/Clothes.js";
 import News from "./webFront/components/news/News.js";
-import Others from "./webFront/components/others/Others.js";
-import SpecialOffers from "./webFront/components/specialOffers/SpecialOffers.js";
-import SeasonsOffers from "./webFront/components/seasonsOffers/SeasonsOffers.js";
+import { createContext, useEffect, useState } from "react";
 
-
-
+export const userInfo = createContext();
 function App() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    if (localStorage.getItem("user") && !user) {
+      setUser(JSON.parse(localStorage.getItem("user")));
+    }
+  }, [user]);
+
   return (
     <div className="App">
-
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Web />} />
-          <Route path="/product/:id" element={<Details />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/basket" element={<Basket />} />
-          <Route path='/showmore' element={<ShowMore />} />
-          <Route path="/seasonsoffers" element={<SeasonsOffers />} />
-          <Route path="/specialoffers" element={<SpecialOffers />} />
-          <Route path="/others" element={<Others />} />
-          <Route path='/cosmetics' element={<Cosmetics />} />
-          <Route path='/skincare' element={<SkinCare />} />
-          <Route path='/clothes' element={<Clothes />} />
-          <Route path="/news" element={<News />} />
-
-
-          
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-
-
+      <userInfo.Provider
+        value={{
+          user,
+          setUser,
+        }}
+      >
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Web />} />
+            <Route path="/product/:id" element={<Details />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/basket" element={<Basket />} />
+            <Route path="/showmore/:category" element={<ShowMore />} />
+            <Route path="/news" element={<News />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </userInfo.Provider>
     </div>
-
   );
 }
 
